bf12fd00b040bdafe4dea2c2c79f01ef305ab5c0
Tiago Barbosa <tiago@farias.cc>
2021-09-09 05:49:20 -0300

    MS Learn projects - Razor Pizzas finished

--- RazorPagesPizza/Pages/Pizza.cshtml.cs
+++ RazorPagesPizza/Pages/Pizza.cshtml.cs
@@ -4,13 +4,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesPizza.Models;
+using RazorPagesPizza.Services;
 
 namespace RazorPagesPizza.Pages
 {
-    public class PizzaModel : PageModel
+  public class PizzaModel : PageModel
+  {
+    [BindProperty] public Pizza NewPizza { get; set; }
+    public List<Pizza> Pizzas { get; set; }
+
+    public void OnGet()
     {
-        public void OnGet()
-        {
-        }
+      Pizzas = PizzaService.GetAll();
     }
+
+    public string GlutenFreeText(Pizza pizza)
+    {
+      if (pizza.IsGlutenFree)
+        return "Gluten Free";
+      return "Not Gluten Free";
+    }
+
+    public IActionResult OnPost()
+    {
+      if (!ModelState.IsValid)
+        return Page();
+
+      PizzaService.Add(NewPizza);
+      return RedirectToAction("Get");
+    }
+
+    public IActionResult OnPostDelete(int id)
+    {
+      PizzaService.Delete(id);
+      return RedirectToAction("Get");
+    }
+  }
 }
