e42af5ed41c4de9a62ec14a667c666491f596ee3
Tiago Barbosa <tiago@farias.cc>
2021-09-15 23:26:56 -0300

    Ajustes

--- DotNetDependencies/Program.cs
+++ DotNetDependencies/Program.cs
@@ -3,28 +3,31 @@ using Humanizer;
 
 namespace DotNetDependencies
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Quantities:");
-            HumanizeQuantities();
+      Console.WriteLine("Quantities:");
+      HumanizeQuantities();
 
-            Console.WriteLine("\nDate/Time Manipulation:");
-            HumanizeDates();
-        }
-
-        static void HumanizeQuantities(){
-            Console.WriteLine("case".ToQuantity(0));
-            Console.WriteLine("case".ToQuantity(1));
-            Console.WriteLine("case".ToQuantity(5));
-        }
-
-        static void HumanizeDates(){
-            Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
-            Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
-            Console.WriteLine(TimeSpan.FromDays(1).Humanize());
-            Console.WriteLine(TimeSpan.FromDays(16).Humanize());
-        }
+      Console.WriteLine("\nDate/Time Manipulation:");
+      HumanizeDates();
     }
+
+    static void HumanizeQuantities()
+    {
+      Console.WriteLine("case".ToQuantity(0));
+      Console.WriteLine("case".ToQuantity(1));
+      Console.WriteLine("case".ToQuantity(5));
+      Console.WriteLine("case".ToQuantity(10));
+    }
+
+    static void HumanizeDates()
+    {
+      Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+      Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+      Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+      Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+    }
+  }
 }
