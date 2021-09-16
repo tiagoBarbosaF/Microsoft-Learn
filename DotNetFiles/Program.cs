5dc5e2083c8fa82cfabe24fc3fe794d880198745
Tiago Barbosa <tiago@farias.cc>
2021-09-16 11:05:52 -0300

    DotNet files module, structure, methods and tips

--- DotNetFiles/Program.cs
+++ DotNetFiles/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -6,17 +7,22 @@ namespace FileModule
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static void Main( string[] args )
         {
-            var salesFiles = FindFiles("stores");
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var storesDirectory = Path.Combine(currentDirectory, "stores");
 
-            foreach (var file in salesFiles)
-            {
-                Console.WriteLine(file);
-            }
+            var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+            Directory.CreateDirectory(salesTotalDir);
+
+            var salesFiles = FindFiles(storesDirectory);
+
+            var salesTotal = CalculateSalesTotal(salesFiles);
+
+            File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"Total Sales for all Stores - {salesTotal:F2}{Environment.NewLine}");
         }
 
-        private static IEnumerable<string> FindFiles(string folderName)
+        private static IEnumerable<string> FindFiles( string folderName )
         {
             var salesFiles = new List<string>();
 
@@ -24,11 +30,33 @@ namespace FileModule
 
             foreach (var file in foundFiles)
             {
-                if(file.EndsWith("sales.json"))
+                var extension = Path.GetExtension(file);
+                if (extension == ".json")
                     salesFiles.Add(file);
             }
 
             return salesFiles;
+        }
+
+
+        static double CalculateSalesTotal( IEnumerable<string> salesFiles )
+        {
+            double salesTotal = 0;
+
+            foreach (var file in salesFiles)
+            {
+                string salesJson = File.ReadAllText(file);
+
+                SalesData data = JsonConvert.DeserializeObject<SalesData>(salesJson);
+
+                salesTotal += data.Total;
+            }
+
+            return salesTotal;
+        }
+        class SalesData
+        {
+            public double Total { get; set; }
         }
     }
 }
