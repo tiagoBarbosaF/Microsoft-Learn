8f920f7a015cbfe94f8413d58adbbf835ca145eb
Tiago Barbosa <tiago@farias.cc>
2021-09-16 06:30:28 -0300

    DotNet Files Module

---
+++ DotNetFiles/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileModule
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var salesFiles = FindFiles("stores");
+
+            foreach (var file in salesFiles)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        private static IEnumerable<string> FindFiles(string folderName)
+        {
+            var salesFiles = new List<string>();
+
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+            foreach (var file in foundFiles)
+            {
+                if(file.EndsWith("sales.json"))
+                    salesFiles.Add(file);
+            }
+
+            return salesFiles;
+        }
+    }
+}
